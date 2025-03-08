4970c3c9f05b5d1c024f6848769bbc2969b06e06
Calvin Henderson <henderson.calvin.w@gmail.com>
2025-03-08 17:20:23 -0500

    Base config for grippy60

---
+++ config/grippy60.h
@@ -0,0 +1,49 @@
+#pragma once
+
+// LAYERS
+#define MACOS 0
+#define LINUX 1
+#define BASE_LAYERS MACOS LINUX
+
+// TIMEOUTS
+#define TIMEOUT_SMALL   80
+#define TIMEOUT_NORMAL 150
+#define TIMEOUT_LONG   250
+#define TIMEOUT_XLONG 1500
+
+// Labels for switch directions according to the matrix
+#define LOTU 0
+#define LOTL 3
+#define LOTC 4
+#define LOTR 5
+#define LOTD 12
+
+#define LITU 1
+#define LITL 6
+#define LITC 7
+#define LITR 8
+#define LITD 13
+
+#define LINU 2
+#define LINL 9
+#define LINC 10
+#define LINR 11
+#define LIND 14
+
+#define LMDU 15
+#define LMDL 18
+#define LMDC 19
+#define LMDR 20
+#define LMDD 27
+
+#define LRGU 16
+#define LRGL 21
+#define LRGC 22
+#define LRGR 23
+#define LRGD 28
+
+#define LPKU 17
+#define LPKL 24
+#define LPKC 25
+#define LPKR 26
+#define LPKD 29
