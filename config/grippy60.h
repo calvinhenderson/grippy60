b6864d7cd315fabaedb89a7460c4c205a93f4bff
Calvin Henderson <henderson.calvin.w@gmail.com>
2025-03-17 22:08:46 -0400

    updated keymap layout to more closely match physical layout

--- config/grippy60.h
+++ config/grippy60.h
@@ -13,76 +13,83 @@
 
 // Labels for switch directions according to the matrix
 
-// Left half
-#define LOTU 0
-#define LOTL 3
-#define LOTC 4
-#define LOTR 5
-#define LOTD 12
+/* FINGER ORIENTATION         x x  */
+/*            ^ [R]ight         x  */
+/*            |                 x  */
+/*  [B]ack    |   [F]orward     x  */
+/*       <----+----->              */
+/*            | \                  */
+/*     [L]eft |  \ [D]own          */
+/*            v   v                */
 
-#define LITU 1
-#define LITL 6
-#define LITC 7
-#define LITR 8
-#define LITD 13
+// Outer thumb
+#define L_OT_F 0
+#define R_OT_F 1
+#define L_OT_L 2
+#define L_OT_D 3
+#define L_OT_R 4
+#define R_OT_L 5
+#define R_OT_D 6
+#define R_OT_R 7
+#define L_OT_B 8
+#define R_OT_B 9
 
-#define LINU 2
-#define LINL 9
-#define LINC 10
-#define LINR 11
-#define LIND 14
+// Inner thumb
+#define L_IT_F 10
+#define R_IT_F 11
+#define L_IT_L 12
+#define L_IT_D 13
+#define L_IT_R 14
+#define R_IT_L 15
+#define R_IT_D 16
+#define R_IT_R 17
+#define L_IT_B 18
+#define R_IT_B 19
 
-#define LMDU 15
-#define LMDL 18
-#define LMDC 19
-#define LMDR 20
-#define LMDD 27
+// Index finger
+#define L_IF_F 20
+#define R_IF_F 21
+#define L_IF_L 22
+#define L_IF_D 23
+#define L_IF_R 24
+#define R_IF_L 25
+#define R_IF_D 26
+#define R_IF_R 27
+#define L_IF_B 28
+#define R_IF_B 29
 
-#define LRGU 16
-#define LRGL 21
-#define LRGC 22
-#define LRGR 23
-#define LRGD 28
+// Middle finger
+#define L_MF_F 30
+#define R_MF_F 31
+#define L_MF_L 32
+#define L_MF_D 33
+#define L_MF_R 34
+#define R_MF_L 35
+#define R_MF_D 36
+#define R_MF_R 37
+#define L_MF_B 38
+#define R_MF_B 39
 
-#define LPKU 17
-#define LPKL 24
-#define LPKC 25
-#define LPKR 26
-#define LPKD 29
+// Ring finger
+#define L_RF_F 40
+#define R_RF_F 41
+#define L_RF_L 42
+#define L_RF_D 43
+#define L_RF_R 44
+#define R_RF_L 45
+#define R_RF_D 46
+#define R_RF_R 47
+#define L_RF_B 48
+#define R_RF_B 49
 
-// Right half
-#define ROTU 30
-#define ROTL 33
-#define ROTC 34
-#define ROTR 35
-#define ROTD 42
-
-#define RITU 31
-#define RITL 36
-#define RITC 37
-#define RITR 38
-#define RITD 43
-
-#define RINU 32
-#define RINL 39
-#define RINC 40
-#define RINR 41
-#define RIND 44
-
-#define RMDU 45
-#define RMDL 48
-#define RMDC 49
-#define RMDR 50
-#define RMDD 57
-
-#define RRGU 46
-#define RRGL 51
-#define RRGC 52
-#define RRGR 53
-#define RRGD 58
-
-#define RPKU 47
-#define RPKL 54
-#define RPKC 55
-#define RPKR 56
-#define RPKD 59
+// Pinky finger
+#define L_PF_F 50
+#define R_PF_F 51
+#define L_PF_L 52
+#define L_PF_D 53
+#define L_PF_R 54
+#define R_PF_L 55
+#define R_PF_D 56
+#define R_PF_R 57
+#define L_PF_B 58
+#define R_PF_B 59
